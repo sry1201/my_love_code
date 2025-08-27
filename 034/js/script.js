bff52a08bf712396c83f01e464a3967bfa19420c
A0061237 <songry@yuanian.com>
2025-08-27 22:41:26 +0800

    背景改动，但是不生效

--- 034/js/script.js
+++ 034/js/script.js
@@ -10,7 +10,10 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  alpha: true
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
